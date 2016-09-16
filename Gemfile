cd940cfa96e807b1a5a8996c97d05987035ac6fa
Tynnee <victorasantekyei@yahoo.co.uk>
2016-09-16 23:55:20 +0200

    partials

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'coffee-rails', '~> 4.2'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
+gem 'requests'
 
 gem 'railties'
 
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,4 +1,13 @@
 class ProfileController < ApplicationController
   def index
+    require "requests"
+
+response = Requests.request("GET", "https://www.googleapis.com/books/v1/volumes?q=ruby+on+rails")
+
+# Now you have these methods available
+
+response.body    #=> String with the response body
+@data = JSON.parse(response.body)
   end
+
 end
